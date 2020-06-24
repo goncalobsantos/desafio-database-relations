3b7f949c98fdcf13165604cb59bfa4defef73922
Gonçalo Santos <goncalobisantos@gmail.com>
2020-06-24 17:07:55 +0100

    Desafio completo Why: * Missing parts TODO This change addresses the need by: * Finalized and completed all the chunks and all tests passed Notes: * Backend of GoRestaurant

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,61 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer with that id does not exist');
+    }
+
+    const productIds = products.map(product => ({ id: product.id }));
+    const productsToUpdate = await this.productsRepository.findAllById(
+      productIds,
+    );
+
+    if (productsToUpdate.length < products.length) {
+      throw new AppError('Product does not exist');
+    }
+
+    const updateProductStock = productsToUpdate.map(updateProduct => {
+      const { quantity } = products.find(
+        p => p.id === updateProduct.id,
+      ) as IProduct;
+
+      return {
+        id: updateProduct.id,
+        quantity: updateProduct.quantity - quantity,
+      };
+    });
+
+    if (updateProductStock.some(p => p.quantity < 0)) {
+      throw new AppError('We do not have enough items to fulfill your order');
+    }
+
+    const orders = await this.ordersRepository.create({
+      customer,
+      products: productsToUpdate.map(p => {
+        const product = products.find(x => x.id === p.id) as IProduct;
+
+        return {
+          quantity: product?.quantity,
+          product_id: p.id,
+          price: p.price,
+        };
+      }),
+    });
+
+    await this.productsRepository.updateQuantity(updateProductStock);
+
+    return orders;
   }
 }
 
